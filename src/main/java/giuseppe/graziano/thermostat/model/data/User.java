9824c26981975514931f2a1ab3f424b104982897
Giuseppe Graziano <g.graziano1@icloud.com>
2018-07-15 19:56:17 +0200

    security

--- src/main/java/giuseppe/graziano/thermostat/model/data/User.java
+++ src/main/java/giuseppe/graziano/thermostat/model/data/User.java
@@ -14,6 +14,14 @@ public class User {
 
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
